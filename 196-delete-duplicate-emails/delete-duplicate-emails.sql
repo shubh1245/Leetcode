5b0cd5b2027d7d06771296576d813097846101b1
Shubham Kumar <SHUBHAMYADUVANSHI5421@GMAIL.COM>
2026-09-20 03:44:10 +0530

    Time: 882 ms (68.46%) | Memory: 0B (100.00%) - LeetSync

--- 196-delete-duplicate-emails/delete-duplicate-emails.sql
+++ 196-delete-duplicate-emails/delete-duplicate-emails.sql
@@ -1,5 +1,5 @@
 # Write your MySQL query statement below
 DELETE p1 FROM Person p1,
-    Person p2
+Person p2
 WHERE
-    p1.Email = p2.Email AND p1.Id > p2.Id
+p1.Email = p2.Email AND p1.Id > p2.Id
